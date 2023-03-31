3322156035b729fe2a8d107cf0b2c5811a4c3f30
ananya-singh7 <ananyasinghhh777@gmail.com>
2023-04-01 01:53:36 +0530

    New Commit

--- lib/authentication/register.dart
+++ lib/authentication/register.dart
@@ -15,40 +15,35 @@ class _RegisterState extends State<Register> {
   final _formKey = GlobalKey<FormState>();
   String email='';
   String password='';
-  String name='';
   String error='';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purpleAccent,
+      resizeToAvoidBottomInset : false,
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
+        backgroundColor: Colors.purple[100],
         elevation: 0.0,
         title: Text('Sign up'),
-        actions: <Widget>[
-          ElevatedButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Sign In'),
-            onPressed: () {
-              widget.toggleView();
-            },)
-        ],
       ),
       body:Container(
           padding:EdgeInsets.symmetric(vertical: 20.0,horizontal: 50.0),
           child: Form(
-            key: _formKey,
+              key: _formKey,
               child: Column(
                 children: [
-                  SizedBox(height: 20.0),
-                  TextFormField(
-                      validator: (val) => val!.isEmpty?'Enter name':null,
-                      onChanged: (val){
-                        setState(() => name=val);
-                      }
+                  Container(
+                    //height: 200,
+                    child: Image.asset("images/register_img1.png"),
                   ),
-                  SizedBox(height: 20.0),
+                  SizedBox(height: 50.0),
                   TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Your Email",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
                       validator: (val) => val!.isEmpty?'Enter email':null,
                       onChanged: (val){
                         setState(() => email=val);
@@ -56,32 +51,54 @@ class _RegisterState extends State<Register> {
                   ),
                   SizedBox(height:20.0),
                   TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Your Password",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                      ),
                       obscureText: true,
                       validator: (val) => val!.length<6?'Enter a password 6+ chars long':null,
                       onChanged: (val){
                         setState(() => password=val);
                       }
                   ),
-                  SizedBox(height:20.0),
+                  SizedBox(height:7.0),
+                  Text(
+                    error,
+                    style: TextStyle(color: Colors.red,fontSize: 14.0),
+
+                  ),
+                  SizedBox(height:8.0),
                   ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.purple[200],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      ),
                       onPressed: () async{
                         if(_formKey.currentState!.validate()){
                           dynamic result = await _auth.registerWithEmailAndPassword(email, password);
                           if(result==null){
-                            setState(()=>error='please type a valid email');
+                            setState(()=>error='Please type a valid Email');
                           }
                         }
                       },
                       child: Text(
                         'Register',
-                        style: TextStyle(color: Colors.white),
+                        style: TextStyle(color: Colors.white,
+                            fontSize: 18),
                       )),
-                  SizedBox(height: 12.0,),
-                  Text(
-                    error,
-                    style: TextStyle(color: Colors.red,fontSize: 14.0),
+                  SizedBox(height: 7.0,),
+                  TextButton(onPressed: () {
+                    widget.toggleView();
+                  },
+                    child: Text(
+                      'Already have an Account? Sign In',
+                      style: TextStyle(color: Colors.purple[200],fontSize: 14.0),
+                    ),)
 
-                  )
                 ],
               )
           )
